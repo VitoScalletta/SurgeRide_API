5d9a884a611a44a463d809eceac4bec8f33ef16f
VitoScalletta <muhammetemre3737@gmail.com>
2026-05-15 11:10:14 +0300

    Faz 1 :Proje iskeleti oluşturuldu dependiciesler eklendi dosya yapısı oluşturuldu proje dockerda ayağa kaldırıldı

---
+++ src/main/java/com/microcommerce/surgeride_api/SurgeRideApiApplication.java
@@ -0,0 +1,13 @@
+package com.microcommerce.surgeride_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgeRideApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurgeRideApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microcommerce/surgeride_api/SurgeRideApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcommerce.surgeride_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurgeRideApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
