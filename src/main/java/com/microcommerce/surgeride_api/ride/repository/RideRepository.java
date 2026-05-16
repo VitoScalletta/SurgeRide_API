8c4eb7a4e7220d2d6ee7a4375fd9d9b5333d3415
VitoScalletta <muhammetemre3737@gmail.com>
2026-05-16 10:28:30 +0300

    Faz 1: Repolar ayarlandı

--- src/main/java/com/microcommerce/surgeride_api/ride/repository/RideRepository.java
+++ src/main/java/com/microcommerce/surgeride_api/ride/repository/RideRepository.java
@@ -1,8 +1,11 @@
 package com.microcommerce.surgeride_api.ride.repository;
 
 import com.microcommerce.surgeride_api.ride.entity.Ride;
+import com.microcommerce.surgeride_api.ride.enums.RideStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RideRepository extends JpaRepository<Ride,Integer> {
+import java.util.List;
 
+public interface RideRepository extends JpaRepository<Ride,Integer> {
+    boolean existsByRiderIdAndStatusIn(Long riderId, List<RideStatus> statuses);
 }
--- src/main/java/com/microcommerce/surgeride_api/user/repository/UserRepository.java
+++ src/main/java/com/microcommerce/surgeride_api/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.microcommerce.surgeride_api.user.repository;
 import com.microcommerce.surgeride_api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
